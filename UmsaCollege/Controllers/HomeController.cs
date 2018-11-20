a4829bb7f0a44e21abae3543f1609ba9d5b86351
Carlos Zabaleta <zabaletacopa@gmail.com>
2018-11-20 02:42:27 -0500

    Handle HTTP post requests for Insert & Display

--- UmsaCollege/Controllers/HomeController.cs
+++ UmsaCollege/Controllers/HomeController.cs
@@ -3,32 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UmsaCollege.Models;
 
-namespace UmsaCollege.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace UmsaCollege.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
             return View();
         }
-        public IActionResult DisplayPage()
-        {
+
+        public IActionResult DisplayPage() {
             ViewBag.Title = "Display";
-            return View();
+            CourseRepository.AddCourses(new Course {
+                Name = "Programming II",
+                Code = "COMP-100",
+                Description = "COMP100 is an introductory course in programming. It includes programming concepts, logic and program structures.",
+                Season = "Winter",
+                Status = "Full"
+            });
+            CourseRepository.AddCourses(new Course {
+                Name = "Web Development",
+                Code = "COMP-110",
+                Description = ">In this first level web course the student will learn how to access the resources of the Internet, use HTML and CSS to publish high-quality Web documents.",
+                Season = "Fall",
+                Status = "Open"
+            });
+            return View(CourseRepository.courses);
         }
-        public IActionResult InsertPage()
-        {
+
+        public IActionResult InsertPage() {
             ViewBag.Title = "Insert";
             return View();
         }
-        public IActionResult DataPage()
-        {
+
+        [HttpPost]
+        public IActionResult InsertPage(Course course) {
+            CourseRepository.AddCourses(course);
+            return View("DisplayPage", CourseRepository.courses);
+        }
+
+        public IActionResult DataPage() {
             ViewBag.Title = "Data";
             return View();
         }
-        public IActionResult UserPage()
-        {
+
+        public IActionResult UserPage() {
             ViewBag.Title = "User";
             return View();
         }
--- UmsaCollege/Startup.cs
+++ UmsaCollege/Startup.cs
@@ -7,20 +7,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace UmsaCollege
-{
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+namespace UmsaCollege {
+    public class Startup {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
