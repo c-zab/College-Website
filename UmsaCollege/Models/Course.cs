0b2c9c33efb705db26bf003f96805d6996b9acbd
Carlos Zabaleta <zabaletacopa@gmail.com>
2018-11-20 02:11:01 -0500

    Inserting shared view for InsertPage & course repo

---
+++ UmsaCollege/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UmsaCollege.Models {
+    public class Course {
+        public int CourseID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public string Description { get; set; }
+
+        public string Season { get; set; }
+
+        public string Status { get; set; }
+    }
+}
---
+++ UmsaCollege/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UmsaCollege.Models {
+    public class Student {
+        public int StudentID { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string StudentCode { get; set; }
+
+        public DateTime DateOfBrith { get; set; }
+
+        public char Gender { get; set; }
+    }
+}
