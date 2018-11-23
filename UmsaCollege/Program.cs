bc48b7efaf293aa7f182fe484b69d17f2b0f4ea4
Carlos Zabaleta <zabaletacopa@gmail.com>
2018-11-23 02:33:56 -0500

    Changing version from 2.0 to 2.1

--- UmsaCollege/Program.cs
+++ UmsaCollege/Program.cs
@@ -13,12 +13,11 @@ namespace UmsaCollege {
         public static void Main(string[] args) {
             BuildWebHost(args).Run();
         }
-
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseDefaultServiceProvider(options => 
-                    options.ValidateScopes = false)
-                .Build();
+            .UseStartup<Startup>()
+            .UseDefaultServiceProvider(options =>
+                options.ValidateScopes = false)
+            .Build();
     }
 }
